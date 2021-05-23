2dffe676c71d692e9b5fed9827bafd829fff94b9
helloree300 <79813648+helloree300@users.noreply.github.com>
2021-05-23 18:00:59 -0500

    Add files via upload

---
+++ ball.js
@@ -0,0 +1,27 @@
+class Ball{
+    constructor(x, y, radius){
+    var options=
+      {
+        frictionAir:0.005,
+        density:1,
+  
+      }
+      this.body=Bodies.circle(x,y,radius,options);
+      this.radius=radius;
+      World.add(world,this.body);
+    }
+  display()
+    {
+      var pos=this.body.position;
+      var angle= this.body.angle;
+      push()
+      translate(pos.x,pos.y);
+      rotate(angle);
+      strokeWeight(5);
+      stroke("black");
+      fill("red");
+      ellipseMode(RADIUS);
+      ellipse(0,0,this.radius,this.radius);
+      pop();
+    }
+  };
